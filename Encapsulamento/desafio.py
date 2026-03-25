d17d7cf55dfd5f3786fec68da8585758157513c0
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2026-03-24 23:33:52 -0300

    Feito

---
+++ Encapsulamento/desafio.py
@@ -0,0 +1,38 @@
+class Produto:
+    def __init__(self,nome,preco,quantidade):
+        self.__nome:str = nome
+        self.__preco:float = preco
+        self.__quantidade_em_estoque:int = quantidade
+    
+    @property
+    def preco(self):
+        return self.__preco
+    @preco.setter
+    def preco(self,valor):
+        if valor <= 0:
+            print("Valor Invalido")
+        else:
+            self.__preco = valor
+    @property
+    def quantidade_em_estoque(self):
+        return self.__quantidade_em_estoque
+    
+    def quantidade_em_estoque(self,valor):
+        if valor <= 0:
+            print("valor invalido")
+        else:
+            self.quantidade_em_estoque = valor
+            
+    def vender(self,quantidade):
+        if 0 < self.__quantidade_em_estoque <= quantidade:
+            self.__quantidade_em_estoque -= quantidade
+        else:
+            print("Não tem estoque suficiente")
+    def repor_estoque(self,quantidade):
+        self.__quantidade_em_estoque += quantidade
+    def get_nome(self):
+        return self.__nome
+    def get_preco(self):
+        return self.__preco
+    def get_quantidade_em_estoque(self):
+        return self.__quantidade_em_estoque
---
+++ Encapsulamento/q2.py
@@ -0,0 +1,22 @@
+class ContaBancaria:
+    def __init__(self,titular):
+        self.titular = titular
+        self.__saldo = 0
+
+  
+    def depositar(self,valor):
+        if valor <= 0:
+            print("Valor de depósito Inválido")
+        else:
+            self.__saldo += valor
+    def sacar(self,valor):
+        if 0 < valor <= self.__saldo:
+            self.__saldo -= valor
+        else:
+            print("Saldo Inválido ou insuficiente")
+    def consultar_saldo(self,):
+        return self.__saldo
+c1 = ContaBancaria("Arthur",)
+c1.depositar(500)
+c1.sacar(200)
+print(f"Saldo atual: {c1.consultar_saldo()}")

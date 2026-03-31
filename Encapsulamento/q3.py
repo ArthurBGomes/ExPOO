bc0b86eb79ed491544c306313275416bdc5da179
ArthurGomes <brito.gomes@escolar.ifrn.edu.br>
2026-03-31 15:34:07 -0300

    up

--- Encapsulamento/q3.py
+++ Encapsulamento/q3.py
@@ -2,10 +2,8 @@ class ContaBancaria:
     def __init__(self,titular):
         self.titular = titular
         self.__saldo = 0
-    @property
     def get_saldo(self,):
         return self.__saldo
-    @get_saldo.setter
     def set_saldo(self,valor):
         if valor > 0:
             self.__saldo =  valor
@@ -27,5 +25,5 @@ c1 = ContaBancaria("Arthur",)
 c1.depositar(500)
 c1.sacar(200)
 print(f"Saldo atual: {c1.consultar_saldo()}")
-c1.set_saldo = 1000
-print(c1.get_saldo)
+c1.set_saldo(1000)
+print(c1.get_saldo())

6e88e0996c53a6662cf5bb02714210043a1e4a0b
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2026-04-10 00:22:53 -0300

    10/04/2026
    - Começo da lista de Herança

---
+++ Herança/q3.py
@@ -0,0 +1,10 @@
+class Animal:
+    def fazer_som(self):
+        print("Um Som Desconhecido")
+class Cachorro:
+    def fazer_som(self):
+        print("Latido")
+a1 = Animal()
+a1.fazer_som()
+c1 = Cachorro()
+c1.fazer_som()

b226bbff326ec38e166934cfc7a61c436a497584
Arthur Gomes <brito.gomes@escolar.ifrn.edu.br>
2026-06-23 16:16:48 -0300

    23/06/2026

--- Recomeço/BancoApp.py
+++ Recomeço/BancoApp.py
@@ -1,7 +1,35 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
 
-
+class Cliente:
+    def __init__(self,nome,cpf,rua,numero,bairro,cidade):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__endereco = Endereco(rua,numero,bairro,cidade)
+    def get_nome(self):
+        return self.__nome
+    def get_cpf(self):
+        return self.__cpf
+    def get_endereco(self):
+        return self.__endereco
+    def exibir_dados(self):
+        return f"{self.__nome},{self.__cpf},{self.__endereco.exibir_dados()}"
+class Endereco:
+    def __init__(self,rua,numero,bairro,cidade):
+        self.__rua = rua
+        self.__numero = numero
+        self.__bairro = bairro
+        self.__cidade = cidade
+    def get_rua(self):
+        return self.__rua 
+    def get_numero(self):
+        return self.__numero
+    def get_bairro(self):
+        return self.__bairro
+    def get_cidade(self):
+        return self.__cidade
+    def exibir_dados(self):
+        return f"{self.__rua},{self.__numero},{self.__bairro},{self.__cidade}"
 class ContaBancaria:
     numeros_contas = []
     contas_duplicada = []

1a55ac883706fc3f93b08fc690346098a0806a4c
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2026-07-08 07:35:05 -0300

    Update ContaSalario.py

--- Recomeço/ContaSalario.py
+++ Recomeço/ContaSalario.py
@@ -1,10 +1,11 @@
 from ContaBancaria import ContaBancaria
 
 class ContaSalario(ContaBancaria):
-    def __init__(self, cliente, numero, saldo,empresa,saques_realizados):
+    def __init__(self, cliente, numero, saldo,empresa,saques_realizados,limite_saques):
         super().__init__(cliente, numero, saldo)
         self.__empresa = empresa
         self.__saques_realizados = saques_realizados
+        self.__limite_saques = limite_saques
 
     def get_tipo_conta(self):
         return "Conta Salário"
